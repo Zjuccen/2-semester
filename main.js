b9586864eaa8a5aab416ec9c7f3e2a5d333bccee
Zjuccen <zjac.nt@gmail.com>
2022-06-08 15:48:06 +0200

    fix header, tilføjet JS

--- main.js
+++ main.js
@@ -12,4 +12,6 @@ function preloadImages(){
 
 }
 
-preloadImages("logo.jpg", "main_bg.jpg", "body_bg.jpg", "header_bg.jpg");
+preloadImages("cropped-Logo-Farve.png", "Om-os-billed.png", "2.png", "1.png", "3.png", "Logo-Farve.png", "sam-and-the-soulmate.jpg", "gammelgaard.png",
+"i-think-ur-awesome.jpg", "addisabababand.jpg", "doessing.jpg", "Kiosk.jpg", "Pecha-Kucha.jpg", "fille.jpg", "i-think-youre-awesome.jpg", "Kiosk-2.jpg",
+"Fille-2.jpg", "Faratuben.jpg", "Sam-and-the-Soulmates-2.jpg", "forside-baggrund.png", "baggrunds-billed.png");
